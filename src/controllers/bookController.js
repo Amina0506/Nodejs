c634f8c3dd6d296fdcfcef03ed2361f61cb160cc
Amina0506 <amina010506@gmail.com>
2025-12-31 19:34:39 +0100

    Eerste entiteit met model, controller en route

---
+++ src/controllers/bookController.js
@@ -0,0 +1,72 @@
+import Book from '../models/book.js';
+
+//Functie om alle boeken op te halen
+export const getAllBooks = async (req, res) => {
+    try{
+        const books = await Book.find()
+
+        res.json(books);
+    } catch (err){
+        res.status(500).json({ message: err.message });
+    }
+};
+
+//Functie om 1 boek op te halen
+export const getBookById = async (req, res) => {
+    try{
+        const book = await Book
+            .findById(req.params.id);
+
+        if(!book){
+            return res.status(404).json({ message: 'Boek niet gevonden' });
+        }
+
+        res.json(book);
+    } catch (err){
+        res.status(500).json({ message: err.message });
+    }
+};
+
+//Functie om een nieuw boek toe te voegen
+export const createBook = async (req, res) => {
+    const { title, author, year, genre, content } = req.body;
+    const book = new Book({ title, author, year, genre, content });
+
+    try{
+        const newBook = await book.save();
+        res.status(201).json(newBook);
+    } catch (err){
+        res.status(400).json({ message: err.message });
+    }
+};
+
+//Functie om boek up te daten
+export const updateBook = async (req, res) => {
+    try{
+        const book = await Book.findById(req.params.id);
+        if(!book){
+            return res.status(404).json({ message: 'Boek niet gevonden' });
+        }
+
+        Object.assign(book, req.body);
+        const updatedBook = await book.save();
+        res.json(updatedBook);
+    } catch (err){
+        res.status(400).json({ message: err.message });
+    }
+};
+
+//Functie om boek te verwijderen
+export const deleteBook = async (req, res) => {
+    try{
+        const book = await Book.findById(req.params.id);
+        if(!book){
+            return res.status(404).json({ message: 'Boek niet gevonden' });
+        }
+
+        await book.remove();
+        res.json({ message: 'Boek is verwijderd' });
+    } catch (err){
+        res.status(500).json({ message: err.message });
+    }
+};
---
+++ src/models/book.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 100,
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    year: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    genre: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+        minLength: 5,
+    }
+});
+
+export default mongoose.model('Book', bookSchema)
