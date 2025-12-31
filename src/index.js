9ee3db867d6f48fac779dd0f8dab8606b1bc5136
Amina0506 <amina010506@gmail.com>
2025-12-31 22:41:11 +0100

    Route toegevoegd in index

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import express from 'express';
 import bookRoutes from './routes/bookRoutes.js';
+import authorRoutes from "./routes/authorRoutes.js";
 
 dotenv.config();
 const app = express();
+
 app.use(express.json());
+
+//Routes
 app.use('/api', bookRoutes);
+app.use('/api', authorRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
